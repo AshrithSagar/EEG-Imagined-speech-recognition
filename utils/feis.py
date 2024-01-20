8306ea789bbdddb33684764b744096b172a94496
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-01-20 17:45:51 +0530

    feis.py unzip_data_eeg(), load_data_eeg(), extract_labels() added

--- utils/feis.py
+++ utils/feis.py
@@ -6,38 +6,25 @@ import os
 import math
 import glob
 import mne
+import subprocess
 import numpy as np
+import pandas as pd
 import scipy.io
+import antropy
 from rich.console import Console
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    TextColumn,
+    TimeRemainingColumn,
+)
 
 from utils.config import line_separator
 
 
-subjects = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "chinese-1",
-    "chinese-2",
-]
+# Subjects: 01-21 and "chinese-1", "chinese-2"
+all_subjects = [str(i).zfill(2) if i <= 21 else f"chinese-{i-21}" for i in range(1, 24)]
 
 
 class FEISDataLoader:
@@ -45,7 +32,101 @@ class FEISDataLoader:
     Load data from FEIS folder
     """
 
-    def __init__(self, data_folder, console=None):
-        self.data_folder = data_folder
-        self.subjects = subjects
+    def __init__(self, data_dir, subjects="all", console=None):
+        """Parameters:
+        - data_dir (str): Path to the data folder.
+        - subjects (list): List of subjects to load. Use "all" for all subjects (default)
+        """
+        self.data_dir = data_dir
+        self.subjects = all_subjects if subjects == "all" else subjects
         self.console = console if console else Console()
+        self.data = {}
+
+    def unzip_data_eeg(self, delete_zip=False):
+        with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TimeRemainingColumn(),
+            transient=True,
+        ) as progress:
+            task_subjects = progress.add_task(
+                "Subjects ...",
+                total=len(self.subjects),
+                completed=1,
+            )
+
+            for subject in self.subjects:
+                subject_dir = os.path.join(self.data_dir, subject)
+
+                for file in sorted(os.listdir(subject_dir)):
+                    if file.endswith(".zip"):
+                        csv_file = os.path.splitext(file)[0] + ".csv"
+                        csv_path = os.path.join(subject_dir, csv_file)
+
+                        if not os.path.exists(csv_path):
+                            command = [
+                                "unzip",
+                                os.path.join(subject_dir, file),
+                                "-d",
+                                subject_dir,
+                            ]
+                            result = subprocess.run(
+                                command,
+                                capture_output=True,
+                                text=True,
+                            )
+                            if result.stderr:
+                                self.console.print(result.stderr)
+
+                        if delete_zip:
+                            os.remove(os.path.join(subject_dir, file))
+                progress.update(task_subjects, advance=1)
+
+    def load_data_eeg(self, epoch_type, verbose=False):
+        with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TimeRemainingColumn(),
+            transient=True,
+        ) as progress:
+            task_subjects = progress.add_task(
+                "Subjects ...",
+                total=len(self.subjects),
+                completed=1,
+            )
+
+            data = []
+            for subject in self.subjects:
+                file = os.path.join(self.data_dir, subject, f"{epoch_type}.csv")
+                if verbose:
+                    self.console.print(f"Loading {subject}/{file} ...")
+                subject_data = pd.read_csv(file)
+                data.append(subject_data)
+                progress.update(task_subjects, advance=1)
+            self.data.update({epoch_type: data})
+        return data
+
+    def extract_labels(self, epoch_type="speaking"):
+        with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TimeRemainingColumn(),
+            transient=True,
+        ) as progress:
+            task_subjects = progress.add_task(
+                "Subjects ...",
+                total=len(self.subjects),
+                completed=1,
+            )
+            labels = []
+            for subject in self.subjects:
+                file = os.path.join(self.data_dir, subject, f"{epoch_type}.csv")
+                numpy_features = np.genfromtxt(file, delimiter=",", dtype=str)
+                eeg_labels = numpy_features[1::1280, 16]
+                labels.append(eeg_labels)
+                progress.update(task_subjects, advance=1)
+            self.labels = labels
+        return labels
