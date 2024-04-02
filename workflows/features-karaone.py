59b160985226331930474765699eb2365383c6e1
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-04-02 20:44:20 +0530

    features-karaone.py d_args

--- workflows/features-karaone.py
+++ workflows/features-karaone.py
@@ -11,17 +11,17 @@ from utils.config import fetch_select, line_separator, load_config
 
 
 if __name__ == "__main__":
-    args = load_config(config_file="config.yaml", key="karaone")
+    d_args = load_config(config_file="config.yaml", key="karaone")
     dataset = fetch_select("dataset", "KaraOne")
 
     dset = dataset(
-        raw_data_dir=args["raw_data_dir"],
-        subjects=args["subjects"],
+        raw_data_dir=d_args["raw_data_dir"],
+        subjects=d_args["subjects"],
         verbose=True,
     )
 
     dset.process_raw_data(
-        save_dir=args["filtered_data_dir"],
+        save_dir=d_args["filtered_data_dir"],
         pick_channels=[-1],
         l_freq=0.5,
         h_freq=50.0,
@@ -29,17 +29,17 @@ if __name__ == "__main__":
         verbose=True,
     )
 
-    dset.process_epochs(epoch_type=args["epoch_type"])
+    dset.process_epochs(epoch_type=d_args["epoch_type"])
     dset.epochs_info(verbose=True)
     labels = dset.all_epoch_labels
 
     dset.extract_features(
-        save_dir=args["features_dir"],
-        epoch_type=args["epoch_type"],
+        save_dir=d_args["features_dir"],
+        epoch_type=d_args["epoch_type"],
         length_factor=0.1,
         overlap=0.5,
     )
 
-    features = dset.load_features(epoch_type=args["epoch_type"], verbose=True)
+    features = dset.load_features(epoch_type=d_args["epoch_type"], verbose=True)
 
     flattened_features, flattened_labels = dset.flatten(features, labels, verbose=True)
