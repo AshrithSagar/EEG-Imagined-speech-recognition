4bbdebc86a752487b87ba7926446cc015b8fae5a
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-04-02 18:51:22 +0530

    config.yaml structure changed;
    _select, classifier main keys added;

--- workflows/ifs-classifier.py
+++ workflows/ifs-classifier.py
@@ -10,7 +10,10 @@ from utils.ifs import InformationSet
 from utils.feis import FEISDataLoader
 from utils.karaone import KaraOneDataLoader
 
-dataset_map = {"feis": FEISDataLoader, "karaone": KaraOneDataLoader}
+dataset_map = {
+    "feis": FEISDataLoader,
+    "karaone": KaraOneDataLoader,
+}
 classifier_map = {
     "RegularClassifier": RegularClassifier,
     "EvaluateClassifier": EvaluateClassifier,
@@ -20,40 +23,41 @@ classifier_map = {
 
 if __name__ == "__main__":
     args = load_config(config_file="config.yaml")
+    c_args = load_config(config_file="config.yaml", key="classifier")
 
-    dataset_name = args.get("dataset")
+    dataset_name = args.get("_select").get("dataset")
     if dataset_name in dataset_map:
-        args = args[dataset_name]
+        d_args = args[dataset_name]
     else:
         raise ValueError("Invalid dataset name")
 
-    for model in args["models"]:
+    classifier_name = args.get("_select").get("classifier")
+    if classifier_name in classifier_map:
+        classifier = classifier_map[classifier_name]
+    else:
+        raise ValueError(f"Invalid classifier name: {classifier_name}")
+
+    for model in c_args["models"]:
         console = Console(record=True)
-        model_dir = os.path.join(args["model_base_dir"], model)
+        model_dir = os.path.join(d_args["model_base_dir"], model)
         Console().rule(title=f"[bold blue3][Model: {model}][/]", style="blue3")
 
         dataset = dataset_map[dataset_name]
         dset = dataset(
-            raw_data_dir=args["raw_data_dir"],
-            subjects=args["subjects"],
+            raw_data_dir=d_args["raw_data_dir"],
+            subjects=d_args["subjects"],
             verbose=True,
             console=console,
         )
 
-        dset.load_features(epoch_type="thinking", features_dir=args["features_dir"])
+        dset.load_features(epoch_type="thinking", features_dir=d_args["features_dir"])
         features, labels = dset.flatten()
 
         features_ifs = InformationSet(features, verbose=True, console=console)
         eff_features = features_ifs.extract_effective_information()
         dset.dataset_info(eff_features, labels)
 
-        classifier_name = args.get("classifier")
-        if classifier_name in classifier_map:
-            classifier = classifier_map[classifier_name]
-        else:
-            raise ValueError(f"Invalid classifier name: {classifier_name}")
-
-        for task in args["tasks"]:
+        for task in d_args["tasks"]:
             console.rule(title=f"[bold blue3][Task-{task}][/]", style="blue3")
             task_labels = dset.get_task(labels, task=task)
             save_dir = os.path.join(model_dir, classifier_name, f"task-{task}")
@@ -65,7 +69,7 @@ if __name__ == "__main__":
                 save_dir=save_dir,
                 test_size=0.2,
                 random_state=42,
-                trial_size=args["trial_size"] or None,
+                trial_size=c_args["trial_size"] or None,
                 features_names=dset.features_names,
                 verbose=True,
                 console=console,
