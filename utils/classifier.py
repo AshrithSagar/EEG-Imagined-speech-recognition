classifier.py ClassifierGridSearch data leakage fix;

utils/classifier.py
@@ -543,32 +543,32 @@ class ClassifierGridSearch(ClassifierMixin):
     ):
         verbose = self.set_verbose(verbose)
 
-        X, y = self.resample(self.X_raw, self.y_raw, sampler)
-
         if self.trial_size is None:
-            self.trial_size = len(X)
+            self.trial_size = len(self.X_raw)
         elif isinstance(self.trial_size, float) and self.trial_size <= 1.0:
-            self.trial_size = int(self.trial_size * len(X))
+            self.trial_size = int(self.trial_size * len(self.X_raw))
 
         # Stratified sampling
         X_train, X_test, y_train, y_test = train_test_split(
-            X,
-            y,
+            self.X_raw,
+            self.y_raw,
             train_size=int(self.trial_size * (1 - self.test_size)),
             test_size=int(self.trial_size * self.test_size),
             random_state=self.random_state,
-            stratify=y,
+            stratify=self.y_raw,
         )
         self.X = np.concatenate((X_train, X_test))
         self.y = np.concatenate((y_train, y_test))
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+        X_train, self.X_test, y_train, self.y_test = train_test_split(
             self.X,
             self.y,
             test_size=self.test_size,
             random_state=self.random_state,
             stratify=self.y,
         )
+
+        self.X_train, self.y_train = self.resample(X_train, y_train, sampler)
 
         self.get_anova_f()
         self.get_scoring(scoring)