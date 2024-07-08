23632206d6e2c59ffc9966dc344bbc115eb38ff5
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-07-08 19:03:42 +0530

    features.py;
    typing;

--- utils/features.py
+++ utils/features.py
@@ -3,6 +3,8 @@ features.py
 Feature Utility scripts
 """
 
+from typing import Callable, List, Tuple, Union
+
 import antropy as ant
 import numpy as np
 from scipy import integrate, stats
@@ -11,52 +13,52 @@ from scipy import integrate, stats
 class FeatureFunctions:
     """Feature functions to be used in the feature extraction process."""
 
-    def __init__(self, sampling_freq):
+    def __init__(self, sampling_freq: Union[int, float]):
         self.sampling_freq = sampling_freq
 
-    def get(self):
+    def get(self) -> Tuple[List[Callable[[np.ndarray], float]], List[str]]:
         """Returns a list of feature functions to be used in the feature extraction process."""
 
-        def mean(x):
+        def mean(x: np.ndarray) -> float:
             return np.mean(x)
 
-        def absmean(x):
+        def absmean(x: np.ndarray) -> float:
             return np.mean(np.abs(x))
 
-        def maximum(x):
+        def maximum(x: np.ndarray) -> float:
             return np.max(x)
 
-        def absmax(x):
+        def absmax(x: np.ndarray) -> float:
             return np.max(np.abs(x))
 
-        def minimum(x):
+        def minimum(x: np.ndarray) -> float:
             return np.min(x)
 
-        def absmin(x):
+        def absmin(x: np.ndarray) -> float:
             return np.min(np.abs(x))
 
-        def minplusmax(x):
+        def minplusmax(x: np.ndarray) -> float:
             return np.max(x) + np.min(x)
 
-        def maxminusmin(x):
+        def maxminusmin(x: np.ndarray) -> float:
             return np.max(x) - np.min(x)
 
-        def curvelength(x):
+        def curvelength(x: np.ndarray) -> float:
             cl = 0
             for i in range(x.shape[0] - 1):
                 cl += abs(x[i] - x[i + 1])
             return cl
 
-        def energy(x):
+        def energy(x: np.ndarray) -> float:
             return np.sum(np.multiply(x, x))
 
-        def nonlinear_energy(x):
+        def nonlinear_energy(x: np.ndarray) -> float:
             # NLE(x[n]) = x**2[n] - x[n+1]*x[n-1]
             x_squared = x[1:-1] ** 2
             subtrahend = x[2:] * x[:-2]
             return np.sum(x_squared - subtrahend)
 
-        def spectral_entropy(x):
+        def spectral_entropy(x: np.ndarray) -> float:
             return ant.spectral_entropy(
                 x,
                 self.sampling_freq,
@@ -65,52 +67,52 @@ class FeatureFunctions:
                 nperseg=len(x),
             )
 
-        def integral(x):
+        def integral(x: np.ndarray) -> float:
             return integrate.simps(x)
 
-        def stddeviation(x):
+        def stddeviation(x: np.ndarray) -> float:
             return np.std(x)
 
-        def variance(x):
+        def variance(x: np.ndarray) -> float:
             return np.var(x)
 
-        def skewness(x):
+        def skewness(x: np.ndarray) -> float:
             return stats.skew(x)
 
-        def kurtosis(x):
+        def kurtosis(x: np.ndarray) -> float:
             return stats.kurtosis(x)
 
-        def sum(x):
+        def sum(x: np.ndarray) -> float:
             return np.sum(x)
 
-        def sample_entropy(x):
+        def sample_entropy(x: np.ndarray) -> float:
             return ant.sample_entropy(x, order=2, metric="chebyshev")
 
-        def permutation_entropy(x):
+        def permutation_entropy(x: np.ndarray) -> float:
             return ant.perm_entropy(x, order=3, normalize=True)
 
-        def svd_entropy(x):
+        def svd_entropy(x: np.ndarray) -> float:
             return ant.svd_entropy(x, order=3, delay=1, normalize=True)
 
-        def approximate_entropy(x):
+        def approximate_entropy(x: np.ndarray) -> float:
             return ant.app_entropy(x, order=2, metric="chebyshev")
 
-        def petrosian_fd(x):
+        def petrosian_fd(x: np.ndarray) -> float:
             return ant.petrosian_fd(x)
 
-        def katz_fd(x):
+        def katz_fd(x: np.ndarray) -> float:
             return ant.katz_fd(x)
 
-        def higuchi_fd(x):
+        def higuchi_fd(x: np.ndarray) -> float:
             return ant.higuchi_fd(x, kmax=10)
 
-        def rootmeansquare(x):
+        def rootmeansquare(x: np.ndarray) -> float:
             return np.sqrt(np.mean(x**2))
 
-        def detrended_fluctuation(x):
+        def detrended_fluctuation(x: np.ndarray) -> float:
             return ant.detrended_fluctuation(x)
 
-        feature_functions = [
+        feature_functions: List[Callable[[np.ndarray], float]] = [
             mean,
             absmean,
             maximum,
@@ -140,6 +142,6 @@ class FeatureFunctions:
             detrended_fluctuation,
         ]
 
-        features_names = [f"{func.__name__}" for func in feature_functions]
+        features_names: List[str] = [f"{func.__name__}" for func in feature_functions]
 
         return feature_functions, features_names
